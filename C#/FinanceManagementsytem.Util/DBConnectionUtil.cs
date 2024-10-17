fca2d1bdf6ded44f8febeca11f3f54d2d9bc9b3b
Anisha0620 <133042067+Anisha0620@users.noreply.github.com>
2024-10-17 17:12:03 +0530

    Add files via upload

---
+++ C#/FinanceManagementsytem.Util/DBConnectionUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Configuration.Internal;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagementsytem.Util
+{
+    public static class DBConnectionUtil
+    {
+       public static SqlConnection GetConnection()
+        {
+            try
+            {
+				string connectionString = ConfigurationManager.ConnectionStrings["MyDbConnection"].ConnectionString;
+				SqlConnection conn = new SqlConnection(connectionString);
+                conn.Open();
+                return conn;
+
+			}
+            catch(SqlException ex ) 
+            {
+                Console.WriteLine(ex.Message );
+                throw;
+            }
+        }
+    }
+}
