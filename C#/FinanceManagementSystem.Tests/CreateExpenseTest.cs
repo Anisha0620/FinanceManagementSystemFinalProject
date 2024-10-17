e8568ea7e75efc0cc935246d4a10ead111ae9b91
Anisha0620 <133042067+Anisha0620@users.noreply.github.com>
2024-10-17 17:24:17 +0530

    Add files via upload

---
+++ C#/FinanceManagementSystem.Tests/CreateExpenseTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using FinanceManagementSystem.BusinessLayer.Repository;
+using FinanceManagementSystem.Entity;
+using System;
+
+namespace FinanceManagementSystem.Tests
+{
+	[TestFixture]
+	public class CreateExpenseTest
+	{
+		private FinanceRepositoryImpl _repository;
+
+		[SetUp]
+		public void Setup()
+		{
+			_repository = new FinanceRepositoryImpl();
+		}
+
+		[Test]
+		public void CreateExpense_ShouldReturnTrue_WhenExpenseIsCreated()
+		{
+			// Arrange
+			var expense = new Expense { UserId = 1, Amount = 500.00M, CategoryId = 2, Date = DateTime.Now, Description = "Groceries" };
+
+			// Act
+			var result = _repository.CreateExpense(expense);
+
+			// Assert
+			Assert.IsTrue(result, "Expense should be created successfully");
+		}
+	}
+}
---
+++ C#/FinanceManagementSystem.Tests/ExceptionTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using FinanceManagementSystem.BusinessLayer.Repository;
+using System;
+
+namespace FinanceManagementSystem.Tests
+{
+	[TestFixture]
+	public class ExceptionTest
+	{
+		private FinanceRepositoryImpl _repository;
+
+		[SetUp]
+		public void Setup()
+		{
+			_repository = new FinanceRepositoryImpl();
+		}
+
+		[Test]
+		public void DeleteUser_ShouldThrowException_WhenUserIdIsInvalid()
+		{
+			// Arrange
+			int invalidUserId = 999;
+
+			// Act & Assert
+			Assert.Throws<Exception>(() => _repository.DeleteUser(invalidUserId), "Exception should be thrown when trying to delete a non-existent user");
+		}
+	}
+}
