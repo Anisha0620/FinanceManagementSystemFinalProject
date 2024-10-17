dc15daacb8bd8987c30375bb4be5497b762843c8
Anisha0620 <133042067+Anisha0620@users.noreply.github.com>
2024-10-17 17:22:01 +0530

    Add files via upload

---
+++ C#/FinaceManagementSystem.Test/FinanceManagementSystem.Tests/CreateUserTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using FinanceManagementSystem.BusinessLayer.Repository;
+using FinanceManagementSystem.Entity;
+
+namespace FinanceManagementSystem.Tests
+{
+	[TestFixture]
+	public class CreateUserTest
+	{
+		private FinanceRepositoryImpl _repository;
+
+		[SetUp]
+		public void Setup()
+		{
+			_repository = new FinanceRepositoryImpl();
+		}
+
+		[Test]
+		public void CreateUser_ShouldReturnTrue_WhenUserIsCreated()
+		{
+			// Arrange
+			var user = new User { Username = "test_user", Password = "test123", Email = "test@example.com" };
+
+			// Act
+			var result = _repository.CreateUser(user);
+
+			// Assert
+			Assert.IsTrue(result, "User should be created successfully");
+		}
+	}
+}
